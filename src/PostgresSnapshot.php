e8b27fd224178b291e0f2ad57ca0ebacca794df7
Tom <26380378+azgooon@users.noreply.github.com>
2024-11-07 11:11:40 +0000

    Update PostgresSnapshot.php
    removes use of Facade

--- src/PostgresSnapshot.php
+++ src/PostgresSnapshot.php
@@ -6,7 +6,6 @@ use Spatie\DbSnapshots\Events\LoadedSnapshot;
 use Spatie\DbSnapshots\Events\LoadingSnapshot;
 use Spatie\DbSnapshots\Snapshot;
 use Weslinkde\PostgresTools\Support\PostgresHelper;
-use Illuminate\Support\Facades\Storage;
 
 use function Laravel\Prompts\spin;
 
@@ -16,7 +15,7 @@ class PostgresSnapshot extends Snapshot
     public function streamToLocalFile($sourceDisk, $sourcePath, $localFilePath)
     {
         // Open a read stream from the source disk
-        $readStream = Storage::disk($sourceDisk)->readStream($sourcePath);
+        $readStream = $sourceDisk->readStream($sourcePath);
 
         if ($readStream === false) {
             throw new \Exception("Failed to open stream for {$sourcePath} on {$sourceDisk} disk.");
@@ -60,7 +59,7 @@ class PostgresSnapshot extends Snapshot
             $dbDumpFilePath = $this->disk->path($this->fileName);
         } else {
             $dbDumpFilePath = rtrim(config('db-snapshots.temporary_directory_path'), '/') . '/' . $this->fileName;
-            $this->streamToLocalFile(config('postgres-tools.disk'), $this->fileName, $dbDumpFilePath);
+            $this->streamToLocalFile($this->disk, $this->fileName, $dbDumpFilePath);
         }
 
         spin(
